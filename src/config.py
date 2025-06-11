794259d239cf28bb3238299b9fc5b941a43a907f
Maksymilian Bilski <maksde25@gmail.com>
2025-06-11 20:57:23 +0200

    Commit drugi

--- src/config.py
+++ src/config.py
@@ -1,14 +1,16 @@
 class Config:
+    WANDB_PROJECT = "hate-speech-classification"
     TOKENIZER_NAME = "allegro/herbert-klej-cased-tokenizer-v1"
     MODEL_NAME = "allegro/herbert-klej-cased-v1"
     MAX_LENGTH = 512
     VAL_SIZE = 0.25
 
-    BATCH_SIZE = 1
+    BATCH_SIZE = 4
     LEARNING_RATE = 2e-5
     NUM_EPOCHS = 3
     WARMUP_STEPS = 500
     EVAL_STEPS = 500
+    LOGGING_STEPS = 15
     SAVE_STEPS = 500
     WEIGHT_DECAY = 0.01
 
